111e211b948fee759e7e7fe6a34864e7f9c78416
a0916105 <whdofjew@gmail.com>
2021-06-07 16:17:25 +0800

    viewBinding

---
+++ app/src/main/java/tw/idv/jew/recyclerviewsample/MainActivity.kt
@@ -0,0 +1,16 @@
+package tw.idv.jew.recyclerviewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tw.idv.jew.recyclerviewsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myTextView.text = "databinding.ActivityMainBinding"
+    }
+}
